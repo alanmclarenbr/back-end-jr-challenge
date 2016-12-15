e76a6f3a9c4e0c3999cc1a669417b4fdc708f7a4
Alan <alan.ses@gmail.com>
2016-12-15 03:34:13 -0200

    criaçao dos metodos de teste

--- test.js
+++ test.js
@@ -1,24 +1,33 @@
 var Leitor = require('./leitor.js');
 var assert = require('assert');
+const ARRAY = [,,,,,];
+const ARRAY_PREENCHIDO = [1,'Alan',9.5,8.2,8.1,8.8];
+describe("Teste da função validaLinha", function () {
+    it("#Linha sem preenchimento", function () {
 
-describe("Teste de leitura do arquivo", function () {
-    describe("#Arquivo vazio", function () {
-        it("", function () {
-
-        });
+        assert.equal(Leitor.validarLinha(ARRAY)[2],0);
+        assert.equal(Leitor.validarLinha(ARRAY)[3],0);
+        assert.equal(Leitor.validarLinha(ARRAY)[4],0);
+        assert.equal(Leitor.validarLinha(ARRAY)[5],0);
     });
 
-    describe("#Arquivo com uma única linha", function () {
-        it("", function () {
-
-        });
+    it("#Linha Com preenchimento", function () {
+        assert.equal(Leitor.validarLinha(ARRAY_PREENCHIDO)[2],9.5);
+        assert.equal(Leitor.validarLinha(ARRAY_PREENCHIDO)[3],8.2);
+        assert.equal(Leitor.validarLinha(ARRAY_PREENCHIDO)[4],8.1);
+        assert.equal(Leitor.validarLinha(ARRAY_PREENCHIDO)[5],8.8);
     });
 
-    describe("Arquivo com múltiplas linhas", function(){
-
-    });
 });
 
+describe("Teste da função calculaMediaAluno", function () {
+    it("#Valores de notas zerados", function () {
+        assert.equal(Leitor.calculaMediaAluno([1,'Alan',0,0,0,0]),0);
+    });
+    it("#Valores de notas preenchidos", function () {
+        assert.equal(Leitor.calculaMediaAluno(ARRAY_PREENCHIDO).toFixed(2),"8.65");
+    })
+});
 
 //Leitor('./test/alunos_singleline.csv');
 
